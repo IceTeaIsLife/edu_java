97af1c71d704edaab5d033025a9fe56aebac54ab
IceTeaIsLife <snide.pancake@gmail.com>
2020-09-12 23:38:33 +0300

    upd

---
+++ Practice_4/src/Circle.java
@@ -0,0 +1,40 @@
+import java.lang.Math;
+
+public class Circle extends Shape {
+	protected double radius;
+
+	Circle() {
+	}
+
+	Circle(double radius) {
+		this.radius = radius;
+	}
+
+	Circle(double radius, String color, boolean filled) {
+		super(color, filled);
+		this.radius = radius;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double getArea() {
+		return Math.PI * Math.pow(radius, 2);
+	}
+
+	@Override
+	public double getPerimeter() {
+		return 2 * Math.PI * radius;
+	}
+
+	@Override
+	public String toString() {
+		return "Circle parameters:\nradius = " + radius + "\ncolor = " + color + "\n filled = " + filled;
+	}
+}
