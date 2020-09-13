6f6211ff46cf78c1c8144c083493bc22d046b272
IceTeaIsLife <snide.pancake@gmail.com>
2020-09-13 12:34:35 +0300

    upd

---
+++ Practice_5/src/MovableCircle.java
@@ -0,0 +1,37 @@
+public class MovableCircle extends Movable {
+	private int radius;
+	private MovablePoint center;
+
+	MovableCircle(int x, int y, int xSpeed, int ySpeed, int radius) {
+		this.center = new MovablePoint(x, y, xSpeed, ySpeed);
+		this.radius = radius;
+	}
+
+	@Override
+	public String toString() {
+		return "MovableCircle{" +
+				"radius=" + radius +
+				", center=" + center +
+				'}';
+	}
+
+	@Override
+	public void moveUp() {
+		this.center.y += this.center.ySpeed;
+	}
+
+	@Override
+	public void moveDown() {
+		this.center.y -= this.center.ySpeed;
+	}
+
+	@Override
+	public void moveLeft() {
+		this.center.x -= this.center.xSpeed;
+	}
+
+	@Override
+	public void moveRight() {
+		this.center.x += this.center.xSpeed;
+	}
+}
