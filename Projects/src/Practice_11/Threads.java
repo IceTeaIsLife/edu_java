3fcd80a78335fe9aa7af2d27ff29ed1a07c79ccf
IceTeaIsLife <snide.pancake@gmail.com>
2020-10-12 15:30:22 +0300

    upd

--- Projects/src/Practice_11/Threads.java
+++ Projects/src/Practice_11/Threads.java
@@ -7,14 +7,25 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Threads {
 	//static AtomicInteger totalSum = new AtomicInteger();
 	static int totalSum;
-	static int[] arrTotalSum = new int[4];
+	//static int[] arrTotalSum = new int[10];
 	static ReentrantLock lock = new ReentrantLock();
 	public static void main(String[] args) throws InterruptedException {
 		long startTime = System.currentTimeMillis();
-		ArrayList<Thread> threads = new ArrayList<>();
-		for (int i = 0; i <  4; i++){
+		for (int i=0;i<10;i++)									//считаем в 1 потоке
+		{
+			work(i, false);
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("One thread:");
+		System.out.println("total time: " + (endTime - startTime));
+		System.out.println("total sum: " + totalSum + "\n");
+
+		totalSum = 0;
+		startTime = System.currentTimeMillis();
+		ArrayList<Thread> threads = new ArrayList<>();				//неправильно считаем в нескольких потоках
+		for (int i = 0; i < 10; i++){
 			final int localI = i;
-			Thread thread = new Thread(() -> work(localI));
+			Thread thread = new Thread(() -> work(localI, false));
 			thread.start();
 			threads.add(thread);
 		}
@@ -22,32 +33,60 @@ public class Threads {
 		for (Thread t : threads) {
 			t.join();
 		}
-		long endTime = System.currentTimeMillis();
+		endTime = System.currentTimeMillis();
+		System.out.println("Multiple threads, but wrong:");
 		System.out.println("total time: " + (endTime - startTime));
-		for (int j=0; j<arrTotalSum.length; j++)
-		{
-			totalSum += arrTotalSum[j];
+		System.out.println("total sum: " + totalSum + "\n");
+		threads.clear();
+
+
+		totalSum = 0;
+		startTime = System.currentTimeMillis(); //считаем правильно в нескольких потоках
+		for (int i = 0; i < 10; i++){
+			final int localI = i;
+			Thread thread = new Thread(() -> work(localI, true));
+			thread.start();
+			threads.add(thread);
 		}
-		System.out.println("total sum: " + totalSum);
+
+		for (Thread t : threads) {
+			t.join();
+		}
+		endTime = System.currentTimeMillis();
+		System.out.println("Multiple threads, but properly:");
+		System.out.println("total time: " + (endTime - startTime));
+		System.out.println("total sum: " + totalSum + "\n");
 	}
 
-	private static void work(int i) {
+	private static void work(int i, boolean properly) {
 		long startTime = System.currentTimeMillis();
-		long result = doHardWork(i * 1000, 100_000_000, i);
+		long result;
+		if (properly) {
+			result = doHardWorkProperly(i * 1000, 100_000_000);
+		}
+		else {
+			result = doHardWork(i * 1000, 100_000_000);
+		}
 		long endTime = System.currentTimeMillis();
 		System.out.println(i + ": " + result + " | " + (endTime-startTime));
 	}
 
-	private synchronized static long doHardWork(int start, int count, int j) {
+	private static long doHardWork(int start, int count) {
 		long a = 0;
 		for (int i = 0; i < count; i++) {
 			a += (start + i) * (start + i) * Math.sqrt(start + i);
-			//lock.lock();
-			//totalSum++;
-			arrTotalSum[j]++;
-			//totalSum.addAndGet(1);
-			//lock.unlock();
+			totalSum++;
 		}
 		return a;
 	}
+
+	private synchronized static long doHardWorkProperly(int start, int count) {
+		long a = 0;
+		for (int i = 0; i < count; i++) {
+			a += (start + i) * (start + i) * Math.sqrt(start + i);
+			totalSum++;
+		}
+		return a;
+	}
+
 }
