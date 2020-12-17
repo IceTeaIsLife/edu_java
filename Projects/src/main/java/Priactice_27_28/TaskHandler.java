d59e9b572e100c83c3aaf8e6d295415bb924ea0d
Stass <snide.pancake@gmail.com>
2020-12-17 19:32:53 +0300

    upd

---
+++ Projects/src/main/java/Priactice_27_28/TaskHandler.java
@@ -0,0 +1,27 @@
+package Priactice_27_28;
+
+public class TaskHandler {
+
+    @ActionAnnotation(type = "sum")
+    public void argsSum(Data data) {
+        int sum = 0;
+        for (int arg :
+                data.getNumbers()) {
+            sum += arg;
+        }
+        System.out.println("Sum equals " + sum);
+    }
+
+    @ActionAnnotation(type = "print")
+    public void printByDelimeter(Data data) {
+        StringBuilder result = new StringBuilder();
+        for (String word :
+                data.getWords()) {
+            result.append(word);
+            if (!(data.getWords().size() - 1 == data.getWords().indexOf(word))) {
+                result.append(data.getDelimeter());
+            }
+        }
+        System.out.println(result);
+    }
+}
